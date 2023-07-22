eab1471dde157ac07befbf40102263c2ed5a9b55
thabzi <138711988+thabzi@users.noreply.github.com>
2023-07-22 16:26:09 +0200

    New update
    Fix of quick start issue and change of the hello word bit

--- app.java
+++ app.java
@@ -1,5 +1,5 @@
-class QuickStart {
-    public static void main (String[] args) {
-        System.out.println("Hello, World.");
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Hello Thabzi");
     }
 }
